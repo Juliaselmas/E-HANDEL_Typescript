d3ce9383de9596d298bcb215ff6e06e39e011f70
Juliaselmas <144105839+Juliaselmas@users.noreply.github.com>
2024-11-25 12:04:59 +0100

    WiP frontend

--- Frontend/src/Components/ItemCard.tsx
+++ Frontend/src/Components/ItemCard.tsx
@@ -1,14 +1,14 @@
 import { Item } from "../Models/Item";
+import styles from "./ItemCard.module.css"
 
 export const ItemCard = ({ item }: {item: Item}) => {
-    //const img: string = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    //const img: string = "/Users/julia/Desktop/KOD/JAVASCRIPT4/E-HANDEL/Frontend/images/xmas-cookies.jpg";
     return (
         <section>
-            <div className="item-card">
+            <div className={styles.itemCard}>
+                <img src={item.image} alt={item.title}/>
                 <h3>{item.title}</h3>
                 <h5>{item.price}</h5>
-               {/* <img src={item.image} alt={item.title}/> */} 
-
             </div>
         </section>
     )
